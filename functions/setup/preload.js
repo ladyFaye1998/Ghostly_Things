d6cf0d1865f7383b1671febcafdd15719e8d26db
Vladislav Sergienko <sergayen@post.bgu.ac.il>
2021-09-12 22:06:12 +0300

    fixing links?

--- functions/setup/preload.js
+++ functions/setup/preload.js
@@ -1,29 +1,29 @@
 /*Here all the media that has to be loaded before the game is preloaded, imgs and sounds*/
 
 const loadImages = () => {
-    soundImg = loadImage('../../assets/images/sound.png');
-    noSoundImg = loadImage('../../assets/images/no-sound.png');
-    flag = loadImage('../../assets/images/flag.svg');
-    life = loadImage('../../assets/images/life.png');
-    winner_background = loadImage('../../assets/images/winBackground.jpg');
+    soundImg = loadImage('/../../assets/images/sound.png');
+    noSoundImg = loadImage('/../../assets/images/no-sound.png');
+    flag = loadImage('/../../assets/images/flag.svg');
+    life = loadImage('/../../assets/images/life.png');
+    winner_background = loadImage('/../../assets/images/winBackground.jpg');
 }
 
 const loadSounds = () => {
-    backgroundMusic = loadSound("../../assets/sounds/music2.mp3");
+    backgroundMusic = loadSound("/../../assets/sounds/music2.mp3");
     backgroundMusic.setVolume(0.008);
 
-    jumpingSound = loadSound("../../assets/sounds/jumpSound.mp3");
+    jumpingSound = loadSound("/../../assets/sounds/jumpSound.mp3");
     jumpingSound.setVolume(0.1);
 
-    fallingSound = loadSound('../../assets/sounds/falling.wav');
+    fallingSound = loadSound('/../../assets/sounds/falling.wav');
     fallingSound.setVolume(0.01);
 
-    enemyBumpSound = loadSound('../../assets/sounds/monster-bump.wav');
+    enemyBumpSound = loadSound('/../../assets/sounds/monster-bump.wav');
     enemyBumpSound.setVolume(0.01);
 
-    eatingSound = loadSound('../../assets/sounds/eat.wav');
+    eatingSound = loadSound('/../../assets/sounds/eat.wav');
     eatingSound.setVolume(0.1);
 
-    fireworkSound = loadSound('../../assets/sounds/fireworks.wav');
+    fireworkSound = loadSound('/../../assets/sounds/fireworks.wav');
     fireworkSound.setVolume(0.01);
 }
